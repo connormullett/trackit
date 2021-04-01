e13d52d06793e9358e3f84f13a1bf32346e2990d
connor <connormullett@gmail.com>
2021-04-01 00:01:14 -0400

    change env var names

--- src/domains/auth/auth.rs
+++ src/domains/auth/auth.rs
@@ -24,20 +24,21 @@ impl<'a, 'r> FromRequest<'a, 'r> for ApiKey {
     fn from_request(request: &'a Request<'r>) -> request::Outcome<ApiKey, ()> {
         let keys: Vec<_> = request.headers().get("Authentication").collect();
         if keys.len() != 1 {
-            return Outcome::Forward(());
+            return Outcome::Failure((Status::BadRequest, ()));
         }
-        match read_token(keys[0]) {
+        match validate_token(keys[0]) {
             Ok(claim) => Outcome::Success(ApiKey(claim)),
             Err(_) => Outcome::Forward(()),
         }
     }
 }
 
-pub fn read_token(key: &str) -> Result<String, String> {
+pub fn validate_token(key: &str) -> Result<String, String> {
+    let secret_key = dotenv::var("JWT_SIGNING_KEY").expect("JWT_SIGNING_KEY is required");
     let token =
         Token::<Header, Registered>::parse(key).map_err(|_| "Unable to parse key".to_string())?;
-    if token.verify(b"secret_key", Sha256::new()) {
-        token.claims.sub.ok_or("Claims not valid".to_string())
+    if token.verify(secret_key.as_bytes(), Sha256::new()) {
+        token.claims.sub.ok_or("Claims are invalid".to_string())
     } else {
         Err("Token not valid".to_string())
     }
